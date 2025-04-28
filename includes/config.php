3c33f29ace3ed55c7806d68a7e068c9284dc1c68
Rado Pereggini <209077761+radopere@users.noreply.github.com>
2025-04-28 10:30:52 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,16 +1,9 @@
 <?php
 
 $USERS = [
-
     'admin' => 'admin',
-
     'user' => 'user',
-
 ];
 
-
-
-define('SESSION_TIMEOUT', 1800); // 30 minut
-
+define('SESSION_TIMEOUT', 600); // 10 minut
 session_start();
-
